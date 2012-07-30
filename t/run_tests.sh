18a29029aa0dc725c7575e8d5c8c23d71c260c1b
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-07-30 13:21:08 +0900

    * t/run_tests.sh: little better way to run test

--- t/run_tests.sh
+++ t/run_tests.sh
@@ -7,14 +7,45 @@
 #
 
 # Description:
-## runs test in this folder
+# runs test in this folder
 
-t_dir=$(cd `dirname $BASH_SOURCE`; pwd)
+export lib_dir=$(cd `dirname $BASH_SOURCE`/..; pwd)
+export t_dir=$(cd `dirname $BASH_SOURCE`; pwd)
 
-for file in `ls $t_dir/*.ncl`; do
-    ncl $file
-done
+## lets have some wrf data
+export wrfout="~/DATA/wrfout.nc"
+
+function usage()
+{
+    echo "pass"
+}
+
+function run_test()
+{
+    test_name=${1%\.*}
+    log_file="/tmp/$test_name".log
+    echo "running test: $test_name"
+    ncl $@ |tee $log_file
+    no_fatal=`grep -e "fatal" $log_file | wc -l`
+    no_warn=`grep -e "Warn" $log_file | wc -l`
+    if [ $? -ne 0 ]; then
+        echo "$no_warn warnings, $no_fatal Fatal errors"
+        echo "test: $test_name failed!"
+    else
+        echo "$no_warn warnings, $no_fatal Fatal errors"
+        echo "test $test_name pass"
+    fi
+}
 
 
+if [ $# -lt 1 ]
+then
+
+    for file in `ls $t_dir/*.ncl`; do
+        run_test $file
+    done
+else
+    run_test $@
+fi
 
 # run_tests.sh ends here
