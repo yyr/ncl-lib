219de0a02294befc488b34126235c7dc4e1ae1ee
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-07-31 00:28:36 +0900

    * t/run_tests.sh: report printing

--- t/run_tests.sh
+++ t/run_tests.sh
@@ -15,33 +15,52 @@ export t_dir=$(cd `dirname $BASH_SOURCE`; pwd)
 ## lets have some wrf data
 export wrfout="~/DATA/wrfout.nc"
 
+
 function usage()
 {
     echo "pass"
 }
 
+function initialize_report()
+{
+    line=$(printf '%0.1s' "-"{1..70})
+    printf "%s\n" $line > $report_file
+    printf "| %-30s| %-10s | %-10s| %-10s| %-10s\n" \
+        "Test Name" "passed" "Warnings" "Fatal" >> $report_file
+    printf "%s\n" $line >> $report_file
+}
+
 function print_report()
 {
-    echo "pass"
+    printf "| %-30s| %-10s | %-10s| %-10d|\n" \
+        "$test_name" "$passed" "$no_warn" "$no_fatal" >> $report_file
+
 }
 
 function run_test()
 {
     test_name=${1%\.*}
+    no_warn=0
+    no_fatal=0
     log_file="/tmp/$test_name".log
+    passed="no"
+
     echo "running test: $test_name"
     ncl $@ |tee $log_file
+
     no_fatal=`grep -e "fatal" $log_file | wc -l`
     no_warn=`grep -i "Warn" $log_file | wc -l`
+
     if [ $? -ne 0 ]; then
-        echo "$no_warn warnings, $no_fatal Fatal errors"
-        echo "test: $test_name failed!"
+        print_report
     else
-        echo "$no_warn warnings, $no_fatal Fatal errors"
-        echo "test $test_name pass"
+        passed="yes"
+        print_report
     fi
 }
 
+report_file="/tmp/$$.log"
+initialize_report
 
 if [ $# -lt 1 ]
 then
@@ -53,4 +72,5 @@ else
     run_test $@
 fi
 
+cat $report_file
 # run_tests.sh ends here
